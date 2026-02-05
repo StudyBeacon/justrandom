65a778c671f31d519029f7b4a1a44aeb47cc0a30
StudyBeacon <yogesrai978@gmail.com>
2026-02-05 15:34:33 +0545

    Remove comments from script.js
    Removed comments for cleaner code.

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 const noBtn = document.getElementById("no");
 const yesBtn = document.getElementById("yes");
 
-// Position "No" button initially
 noBtn.style.left = "50%";
 noBtn.style.top = "200px";
 
-// Move "No" button when hovered
 noBtn.addEventListener("mouseover", () => {
   const x = Math.floor(Math.random() * (window.innerWidth - 100));
   const y = Math.floor(Math.random() * (window.innerHeight - 50));
@@ -13,7 +11,6 @@ noBtn.addEventListener("mouseover", () => {
   noBtn.style.top = y + "px";
 });
 
-// Action when "Yes" is clicked
 yesBtn.addEventListener("click", () => {
   alert("Yay! ❤️ I knew you'd say YES!");
 });
