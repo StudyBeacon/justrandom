613cda336fa1fb760ee31ce29607afd60fb21271
Yogesh Rai <yogesrai978@gmail.com>
2026-02-14 21:26:47 +0545

    error fixed

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+// Get the Yes and No buttons
 const noBtn = document.getElementById("no");
 const yesBtn = document.getElementById("yes");
 
+// Position "No" button initially
 noBtn.style.left = "50%";
 noBtn.style.top = "200px";
 
+// Make "No" button run away when hovered
 noBtn.addEventListener("mouseover", () => {
   const x = Math.floor(Math.random() * (window.innerWidth - 100));
   const y = Math.floor(Math.random() * (window.innerHeight - 50));
@@ -11,7 +14,7 @@ noBtn.addEventListener("mouseover", () => {
   noBtn.style.top = y + "px";
 });
 
+// When "Yes" is clicked, go to love.html
 yesBtn.addEventListener("click", () => {
-  window.location.href = "love.html"; // go to your special page
+  window.location.href = "love.html"; // redirect to your special page
 });
-
